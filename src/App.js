3ff340048bd1ba2025382b2ac1b2ba3ab3384eb3
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-11-16 21:05:34 -0800

    datagrid styles

--- src/App.js
+++ src/App.js
@@ -51,21 +51,6 @@ function App() {
             </Routes>
               </div>
             </div>
-            
-            {/* <SideBar /> */}
-            {/* <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/contacts" element={<Contacts />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/geography" element={<Geography />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/calendar" element={<Calendar />} />
-            </Routes> */}
           </main>
           
         </div>
--- src/scenes/Team.jsx
+++ src/scenes/Team.jsx
@@ -1,28 +1,109 @@
 import React from 'react'
-import {Box, Typography, useTheme} from '@mui/material';
-import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import {Box, Typography, useTheme, Button, IconButton } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
 import { tokens } from '../theme';
 import { mockDataTeam } from '../data/mockData';
+import Header from '../components/Header';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlined';
+import SupervisorAccountOutlinedIcon from '@mui/icons-material/SupervisorAccountOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlined';
+import SecurityOutlinedIcon from '@mui/icons-material/SecurityOutlined';
 
 
-const rows = mockDataTeam;
 
-const columns = [
-  { field: 'id', headerName: 'Id', width: 150 },
-  { field: 'name', headerName: 'Name', width: 150 },
-  { field: 'email', headerName: 'Email', width: 150 },
-  { field: 'age', headerName: 'Age', width: 150 },
-  { field: 'phone', headerName: 'Phone', width: 150 },
-  { field: 'access', headerName: 'Access', width: 150 },
-];
+
 
 const Team = () => {
+
+  const {palette} = useTheme();
+  const colors = tokens(palette.mode);
+
+  const rows = mockDataTeam;
+
+const columns = [
+  { 
+    field: 'id', 
+    headerName: 'ID', 
+    width: 150,
+    headerClassName: 'super-app-theme--header',
+  },
+  { 
+    field: 'name', 
+    headerName: 'Name', 
+    flex: 1, 
+    cellClassName: 'name-column--cell',
+    headerClassName: 'super-app-theme--header',
+  },
+  { 
+    field: 'email', 
+    headerName: 'Email', 
+    flex: 1, 
+    cellClassName: 'email-column--cell',
+    headerClassName: 'super-app-theme--header',
+  },
+  { 
+    field: 'age', 
+    headerName: 'Age', 
+    flex: 1,
+    type: 'number',
+    headerAlign: 'left',
+    align: 'left', 
+    cellClassName: 'age-column--cell',
+    headerClassName: 'super-app-theme--header',
+  },
+  { 
+    field: 'phone', 
+    headerName: 'Phone', 
+    flex: 1, 
+    cellClassName: 'field-column--cell',
+    headerClassName: 'super-app-theme--header',
+  },
+  { 
+    field: 'access', 
+    headerName: 'Access Level', 
+    flex: 1, 
+    cellClassName: 'access-column--cell',
+    headerClassName: 'super-app-theme--header',
+    renderCell: ({row: {access}}) => {
+      return (
+        <Box 
+          width='80%'
+          m='0 auto'
+          p='5px'
+          display='flex'
+          justifyContent={'center'}
+          backgroundColor={access === 'admin' ? colors.greenAccent[600] : colors.greenAccent[700]}
+        >
+          <IconButton>
+            {access === 'admin' && <AdminPanelSettingsOutlinedIcon />}
+            {access === 'manager' && <SecurityOutlinedIcon />}
+            {access === 'user' && <LockOpenOutlinedIcon />}
+            <Typography color={colors.gray[100]} sx={{ml: '5px'}}>
+              {access}
+            </Typography>
+          </IconButton>       
+        </Box>
+      )
+    }
+  },
+];
+
   return (
-    <Box height='300px' width='100%'>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-      />
+    <Box m='20px'>
+      <Header title={'TEAM'} subTitle={'Managing team members'} />
+      <Box m='40px 0 0 0' height='70vh' width='100%' sx={{
+        '.super-app-theme--header': {
+          backgroundColor: colors.redAccent[300],
+          color: colors.primary[400]
+        }
+      }}>
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          />
+        </Box>
     </Box>
   )
 }
