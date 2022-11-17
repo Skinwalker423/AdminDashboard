f453aa22b82eac9dd70ec3a12e21037a538160d8
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-11-16 17:10:02 -0800

    DataGrid

--- src/scenes/Team.jsx
+++ src/scenes/Team.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import {Box, Typography, useTheme} from '@mui/material';
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { tokens } from '../theme';
+import { mockDataTeam } from '../data/mockData';
+
+
+const rows = mockDataTeam;
+
+const columns = [
+  { field: 'id', headerName: 'Id', width: 150 },
+  { field: 'name', headerName: 'Name', width: 150 },
+  { field: 'email', headerName: 'Email', width: 150 },
+  { field: 'age', headerName: 'Age', width: 150 },
+  { field: 'phone', headerName: 'Phone', width: 150 },
+  { field: 'access', headerName: 'Access', width: 150 },
+];
 
 const Team = () => {
   return (
-    <div>Team</div>
+    <Box height='300px' width='100%'>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+      />
+    </Box>
   )
 }
 
