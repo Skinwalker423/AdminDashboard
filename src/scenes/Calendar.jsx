a212ec818b3e84ace74cdb47382e2b99f48ef9bf
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-11-19 22:26:53 -0800

    handleDateClick handleEventClick

--- src/scenes/Calendar.jsx
+++ src/scenes/Calendar.jsx
@@ -1,13 +1,60 @@
 import React, {useState} from 'react'
 import FullCalendar from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
-import {Box, Typography} from '@mui/material'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import listPlugin from '@fullcalendar/list'
+import interactionPlugin from '@fullcalendar/interaction'
+import {
+  Box, 
+  Typography, 
+  List, 
+  ListItem, 
+  ListItemText, 
+  useTheme
+} from '@mui/material'
+import Header from '../components/Header'
+import { tokens } from '../theme'
 
 const Calendar = () => {
+
+  const {palette} = useTheme();
+  const colors = tokens(palette.mode);
+  const [currentEvents, setCurrentEvents] = useState([]);
+
+  const handleDateClick = (selected) => {
+    const title = prompt("enter title");
+    const calendarApi = selected.view.calendar;
+    calendarApi.unselect();
+
+    if(title){
+      calendarApi.addEvent({
+        id: `${selected.dateStr}-${title}`,
+        title,
+        start: selected.startStr,
+        end: selected.endStr,
+        allDay: selected.allDay,
+      })
+    }
+  }
+
+  const handleEventClick = (selected) => {
+    if(window.confirm(`Are you sure you want to remove event ${selected.event.title}`)){
+      selected.event.remove();
+    }
+  }
+
   return (
     <Box>
+      <Header title='CALENDAR' subTitle='schedule appointments and events' />
       <FullCalendar
-        plugins={[ dayGridPlugin ]}
+        plugins={
+          [ 
+            dayGridPlugin, 
+            timeGridPlugin, 
+            listPlugin, 
+            interactionPlugin
+           ]
+        }
         initialView="dayGridMonth"
       />
     </Box>
