be7e7496a9fe44171b0e954bfea756f2692f7031
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-11-20 22:05:18 -0800

    calendar plugins

--- src/scenes/Calendar.jsx
+++ src/scenes/Calendar.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
+import FullCalendar, { formatDate } from '@fullcalendar/react' // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid'
 import listPlugin from '@fullcalendar/list'
@@ -44,19 +44,83 @@ const Calendar = () => {
   }
 
   return (
-    <Box>
+    <Box m='20px'>
       <Header title='CALENDAR' subTitle='schedule appointments and events' />
-      <FullCalendar
-        plugins={
-          [ 
-            dayGridPlugin, 
-            timeGridPlugin, 
-            listPlugin, 
-            interactionPlugin
-           ]
-        }
-        initialView="dayGridMonth"
-      />
+      <Box display='flex' justifyContent='space-between' >
+        <Box 
+          flex='1 1 20%'
+          backgroundColor={colors.primary[400]}
+          p='15px'
+          borderRadius='4px'
+          border='2px solid white'
+
+        >
+          <Typography variant='h5'>Events</Typography>
+          <List>
+            {currentEvents.map((event) => {
+              return (
+                <ListItem
+                  key={event.id}
+                  sx={{
+                    backgroundColor: colors.greenAccent[500],
+                    margin: '10px 0',
+                    borderRadius: '2px',
+                  }}
+                >
+                  <ListItemText
+                    primary={event.title}
+                    secondary={
+                      <Typography>
+                      {formatDate(event.start, {
+                        year: 'numeric',
+                        month: 'short',
+                        year: 'numeric',
+                      })}
+                      </Typography>
+                    }
+                  />  
+                </ListItem>
+              )
+            })}
+          </List>
+        </Box>
+        <Box flex='1 1 100%' ml='20px'>
+          <FullCalendar
+            height={'80vh'}
+            plugins={[dayGridPlugin,timeGridPlugin,listPlugin,interactionPlugin]}
+            headerToolbar={{
+              left: 'prev,next today',
+              center: 'title',
+              right: "dayGridPlugin,timeGridPlugin,listPlugin,interactionPlugin"
+            }}
+            initialView="dayGridMonth"
+            editable={true}
+            selectable={true}
+            selectMirror={true}
+            dayMaxEvents={true}
+            select={handleDateClick}
+            eventClick={handleEventClick}
+            eventsSet={(events) => setCurrentEvents(events)}
+            initialEvents={[
+              {
+              id: '1234',
+              title: 'All-day event',
+              date: '2022-11-25',
+              },
+              {
+              id: 'event2',
+              title: 'meeting',
+              date: '2022-11-26',
+              },
+              {
+              id: 'event3',
+              title: 'party',
+              date: '2022-11-27',
+              },
+          ]}
+          />
+        </Box>
+      </Box>
     </Box>
   )
 }
