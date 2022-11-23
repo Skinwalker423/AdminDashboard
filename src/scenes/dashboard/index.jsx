6d5dd55a511eae859df42013296025f4f5f80495
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-11-23 01:00:06 -0800

    stats

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -46,8 +46,84 @@ const Dashboard = () => {
           </Button>
         </Box>
       </Box>
-      <Box>
-        test
+      <Box 
+        display='grid'
+        gridTemplateColumns={'repeat(12, 1fr)'}
+        gridAutoRows='140px'
+        gap='20px'
+      >
+        <Box 
+          backgroundColor={colors.primary[400]} 
+          display='flex'
+          gridColumn={'span 3'}
+          justifyContent='center'
+          alignItems='center'
+        >
+          <StatBox
+            title={'12,361'}
+            subTitle={'Emails Sent'}
+            progress={0.75}
+            increase={'+14%'}
+            icon={<EmailOutlinedIcon sx={{
+              color: colors.greenAccent[600],
+              fontSize: '26px'
+            }} />}
+          />
+        </Box>
+        <Box 
+          backgroundColor={colors.primary[400]} 
+          display='flex'
+          gridColumn={'span 3'}
+          justifyContent='center'
+          alignItems='center'
+        >
+          <StatBox
+            title={'32,441'}
+            subTitle={'New Clients'}
+            progress={0.30}
+            increase={'+58%'}
+            icon={<PersonAddAlt1OutlinedIcon sx={{
+              color: colors.greenAccent[600],
+              fontSize: '26px'
+            }} />}
+          />
+        </Box>
+        <Box 
+          backgroundColor={colors.primary[400]} 
+          display='flex'
+          gridColumn={'span 3'}
+          justifyContent='center'
+          alignItems='center'
+        >
+          <StatBox
+            title={'431,225'}
+            subTitle={'Sales Obtained'}
+            progress={0.50}
+            increase={'+25%'}
+            icon={<PointOfSaleOutlinedIcon sx={{
+              color: colors.greenAccent[600],
+              fontSize: '26px'
+            }} />}
+          />
+        </Box>
+        <Box 
+          backgroundColor={colors.primary[400]} 
+          display='flex'
+          gridColumn={'span 3'}
+          justifyContent='center'
+          alignItems='center'
+        >
+          <StatBox
+            title={'1,325,134'}
+            subTitle={'Traffic Inbound'}
+            progress={0.80}
+            increase={'+43%'}
+            icon={<TrafficOutlinedIcon sx={{
+              color: colors.greenAccent[600],
+              fontSize: '26px'
+            }} />}
+          />
+        </Box>
       </Box>
     </Box>
   )
