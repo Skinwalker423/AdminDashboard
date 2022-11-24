4bbcb28128a5660135cd07dd3530726c76a0981b
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-11-23 22:13:06 -0800

    Transaction row

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -163,14 +163,79 @@ const Dashboard = () => {
                   fontSize: '26px',
                   color: colors.greenAccent[500]
                 }}
-               />
+              />
             </IconButton>
           </Box>  
         </Box>
-          <Box height='250px' ml='-20px'>
+          <Box height='250px' mt='-20px'>
             <LineChart isDashBoard={true} />
           </Box>
-        </Box>
+      </Box>
+
+        {/* Transactions */}
+          <Box 
+            gridColumn={'span 4'} 
+            gridRow='span 2' 
+            backgroundColor={colors.primary[400]}
+            overflow='auto'
+          >
+            <Box
+              display='flex'
+              justifyContent='space-between'
+              alignItems='center'
+              borderBottom={`4px solid ${colors.primary[500]}`}
+              colors={colors.gray[100]}
+              p='15px'
+            >
+              <Typography 
+                color={colors.gray[100]}
+                variant='h5'
+                fontWeight='600'
+              >
+                Recent Transactions
+              </Typography>            
+            </Box>
+            {mockTransactions.map((trans, index) => {
+              return (
+                <Box
+                  key={`${trans.txId}-${index}`}
+                  justifyContent='space-between'
+                  alignItems='center'
+                  borderBottom={`4px solid ${colors.primary[500]}`}
+                  p='15px'
+                  
+                >
+                  <Box display='flex' justifyContent='space-between'>
+                    <Box>
+                      <Typography>
+                        {trans.txId}
+                      </Typography>
+                      <Typography color={colors.greenAccent[500]}>
+                        {trans.user}
+                      </Typography>
+                    </Box> 
+                    <Box
+                      color={colors.gray[100]}
+                      display='flex'
+                      alignItems='center'
+                    >
+                      {trans.date}
+                    </Box>   
+                    <Box
+                      backgroundColor={colors.greenAccent[600]}
+                      p='5px 10px'
+                      borderRadius='4px'
+                      display='flex'
+                      alignItems='center'
+                      
+                    >
+                      {`$${trans.cost}`}
+                    </Box>  
+                  </Box>  
+                </Box>
+              )
+            })}
+          </Box>
       </Box>
     </Box>
   )
