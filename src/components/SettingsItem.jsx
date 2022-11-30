731456c173d39131d28385bdb4d30237dbe70de9
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-11-29 19:26:34 -0800

    settings menu item

---
+++ src/components/SettingsItem.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Box, Typography, IconButton } from '@mui/material'
+import { useColors } from '../hooks'
+
+const SettingsItem = ({title, icon}) => {
+
+  const {colors} = useColors();
+
+  return (
+    <Box 
+        borderBottom={'2px solid white'}
+        m='20px'
+        display='flex'
+        justifyContent={'space-between'}
+        alignItems={'center'}
+            
+    >
+        <Typography
+            color={colors.gray[100]}
+            variant={'h4'}
+            
+        >
+            {title}
+        </Typography>
+        <IconButton>
+            {icon}
+        </IconButton>
+    </Box>
+  )
+}
+
+export default SettingsItem
